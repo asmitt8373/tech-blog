c26e0afc3e25c872e74d6c883e028fd115d38013
asmitt8373 <austinsmittle8373@gmail.com>
2022-09-22 18:06:44 -0700

    fixed the routes and apis

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {}
-}
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {}
+// }
+// sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+      sequelize.sync({force: false})
+    });
+//   });
